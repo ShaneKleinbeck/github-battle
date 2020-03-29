f0aa4e90531fb305ee04d8350d7469c4d0091131
Shane Kleinbeck <shanekleinbeck@gmail.com>
2020-03-29 18:26:55 -0400

    created dist folder with index_bundle.js file. Updated webpack config with inde_bundle output

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,12 @@ module.exports = {
     module: {
         rules: [
            { test: /\.svg$/, use: 'svg-inline-loader' },
-           { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] } 
+           { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }, 
            { test: /\.(js)$/, use: 'babel-laoder' }
         ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
     }
 }
